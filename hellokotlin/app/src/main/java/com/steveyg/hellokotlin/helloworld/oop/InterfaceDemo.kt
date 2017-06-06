a33d1eb78d8953b3dc5d263b488bf2423c0d26ba
steveyg <yangguang@iscas.ac.cn>
2017-06-06 18:18:34 +0800

    接口

---
+++ hellokotlin/app/src/main/java/com/steveyg/hellokotlin/helloworld/oop/InterfaceDemo.kt
@@ -0,0 +1,20 @@
+package com.steveyg.hellokotlin.helloworld.oop
+
+import com.steveyg.hellokotlin.helloworld.mvp.model.MyModel
+
+/**
+ * Created by lenovo on 2017/6/6.
+ */
+
+interface mInterfaceDemo{
+    fun function1()
+    fun function2(){
+
+    }
+}
+class InterfaceDemoImpl : mInterfaceDemo{
+    override fun function1() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
